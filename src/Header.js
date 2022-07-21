7737d014c41e70412b5c636986e59e729ec296fb
Andy-Intelligence <usoroandidiong@gmail.com>
2022-07-21 02:17:08 -0700

    done
    done

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {Avatar} from "@mui/material"
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <div className = "header">
+      <Link to = "/">
+        <img className="header__icon" src="https://clipart.info/images/ccovers/1499955328airbnb-2-logo-png.png" alt =""/>
+        </Link>
+
+
+        <div className='header__center'>
+            <input type = 'text'/>
+            <SearchIcon/>
+        </div>
+
+
+        <div className='header__right'>
+            <p>Become a host</p>
+            
+            <LanguageIcon/>
+            <ExpandMoreIcon/>
+            <Avatar/>
+        </div>
+    </div>
+
+     
+  )
+}
+
+export default Header
